f0dd6eface83ded571b94553c437045a7dc9f9dd
techno156 <techno156@hotmail.com>
2022-05-04 01:41:02 +1000

    Improved commenting. Added function to check whether a table exists in the datbase. Updated function that creates tables, so that it checks whether the table existsf first, and will return an appropriate result, if it can create the table. Started work on function to fetch data, and main script function.

--- sqlite.js
+++ sqlite.js
@@ -1,35 +1,61 @@
 //Sqlite File
 const sqlite3 = require('sqlite3').verbose(); //Create global variable for SQLite
 
-function connectDatabase() { //Open and connect to the database, if it does not exist  
-    var databaseFileName = "secrets";
+
+//--- Core Database Functions ---
+function connectDatabase(databaseFileName) { //Open and connect to the database, if it does not exist  
     var db = new sqlite3.Database(databaseFileName);
-    return db;
+    return db; //Return database object. Don't forget to close!
 }
 
 function closeDatabase(db){ //Close the database
-    if (db) {
+    if (db) { //Only run if the database is open
         db.close();
     }
 }
 
+//--- Table Creation Functions ---
+
 function checkTableExists(db, table) {
     //check whether the table exists 
-    db.serialize(() => {
-        db.get //Return PRAGMA for table, and check the number of entries returned to get table name (https://stackoverflow.com/questions/59514987/how-to-check-if-a-table-exists-in-sqlite3-nodejs) https://www.sqlite.org/pragma.html#pragma_table_info
-    })
+    db.get(`SELECT name FROM sqlite_master WHERE type='table' AND name=?`, table, (err, row) => {
+        if (err) {
+            console.error("SQL table Check error: " + err);
+            return;
+        }
+
+        if (row) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }); //Return PRAGMA for table, and check the number of entries returned to get table name (https://stackoverflow.com/questions/59514987/how-to-check-if-a-table-exists-in-sqlite3-nodejs) https://www.sqlite.org/pragma.html#pragma_table_info
 }
 
 function createSecretTables(db) { //Create the secret tables for the database. DOES NOT CREATE SHARD DATABASE
-    // var tableExists = false; //Just use the checkTableExists function
+    var tableExists = checkTableExists(db, "Secrets"); 
 
-    db.serialize(() => {
-        db.prepare(`CREATE TABLE IF NOT EXISTS 'Secrets'(
+    if (!tableExists) { //Create table only if it does not exist
+        db.run(`CREATE TABLE IF NOT EXISTS 'Secrets'(
         secret_id TEXT PRIMARY KEY NOT NULL,
         passphrase TEXT NOT NULL,
         ExpiryDate DATE,
-        Viewed BOOLEAN NOT NULL)`) //insert create statements for table here
-    })
+        Viewed BOOLEAN NOT NULL)`); //Run create statement (Remove Viewed Boolean? The secret can just be deleted when viewed)
+    }
+    return tableExists;
 }
 
-//TODO: Write function to perform actual database operations and call the above functions to do it with 
+// --- Secret Storage Functions
+
+function getRow(db, table) { //Fetch data from row
+    
+}
+
+//--- Main Page Functions ---
+function main() {
+    var databaseFileName = "secrets.db"; //Database filename
+    db = connectDatabase(databaseFileName); //Create Database Object
+
+    closeDatabase(db); //Close Database when finished
+}
