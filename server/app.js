f5e63e994d58f162b27b4ce8b02b0e3c423d0ce3
Mitchell Thomas <6386297+mitchts@users.noreply.github.com>
2022-05-17 10:57:48 +1000

    Add base application structure

---
+++ server/app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const helmet = require('helmet')
+const morgan = require('morgan')
+const errorhandler = require('errorhandler')
+
+const isProduction = process.env.NODE_ENV === 'production'
+
+const app = express()
+
+const db = require('./services/db')
+
+app.use(helmet())
+app.use(bodyParser.json())
+app.use(cors())
+app.use(morgan('combined'))
+app.use(express.static(__dirname + '/public'))
+
+let server = app.listen(process.env.PORT || 3001, () => {
+  console.log('listening on port ' + server.address().port)
+})
---
+++ server/services/db.js
@@ -0,0 +1,25 @@
+// database connection
+const sqlite3 = require('sqlite3')
+const db = new sqlite3.Database('essw.db')
+
+db.serialize(() => {
+  db.run(`BEGIN EXCLUSIVE TRANSACTION`)
+  db.run(`CREATE TABLE if not exists SECRETS (
+    id TEXT PRIMARY KEY NOT NULL,
+    store TEXT NOT NULL,
+    encrypted_passphrase TEXT NOT NULL,
+    method INTEGER NOT NULL,
+    expiration DATE,
+    failed_attempts INTEGER NOT NULL DEFAULT 0,
+    locked_at DATE
+  )`, (err) => {
+    if (err) {
+      console.error(err)
+      return db.run(`ROLLBACK`)
+    }
+  })
+
+  db.run(`COMMIT`)
+})
+
+module.exports = db
