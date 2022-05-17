e747572f248535ba152326b6fd226dfb5d3b154a
Mitchell Thomas <6386297+mitchts@users.noreply.github.com>
2022-05-17 15:44:35 +1000

    Move crypto test out of Secrets route

--- server/routes/v1/index.js
+++ server/routes/v1/index.js
@@ -2,6 +2,7 @@ var router = require('express').Router()
 
 router.use('/secret', require('./secret'))
 router.use('/passphrase', require('./passphrase'))
+router.use('/test', require('./crypto_test'))
 
 router.get('/', (req, res) => {
   res.send({'status': 200})
