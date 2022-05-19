5128a0821d5d40198bb9dbe4d29082f54a80b67c
Petri Bayley <petribayley@gmail.com>
2022-05-19 17:49:01 +1000

    Added comment to the /api route

--- node_docker/node_app/index.js
+++ node_docker/node_app/index.js
@@ -1,17 +1,23 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const expressApp = express();
 
-expressApp.post('/api', (request, response) => {
-  response.json({ message: 'OK', code: 200 });
+/*
+  v1.0.1 - Added comment
+  TODO: Add feature
+
+*/
+expressApp.post("/api", (request, response) => {
+  response.json({ message: "OK", code: 200 });
+  //Change
 });
 
-expressApp.get('*', (request, response) => {
-  const url = request.originalUrl.split('?').shift();
+expressApp.get("*", (request, response) => {
+  const url = request.originalUrl.split("?").shift();
   response.sendFile(path.join(__dirname, `/www${url}`));
 });
 
 expressApp.listen(8080, () => {
-  console.log('Listening on port 8080');
+  console.log("Listening on port 8080");
 });
