59303ea7efcdec60801274423da9dc79077309ef
Mitchell Thomas <6386297+mitchts@users.noreply.github.com>
2022-05-20 23:16:42 +1000

    Add database implementation
    So, this isn't going to work how we though. The database code either needs to incorporate callbacks or promises. What has been implemented here is horrid and needs to be completely redone. The code Kevin wrote could not be implemented in any functional way, but designs have been copied over where possible. To be discussed Sunday meeting.

--- server/routes/v1/secret.js
+++ server/routes/v1/secret.js
@@ -2,18 +2,78 @@ var router = require('express').Router()
 var bodyParser = require('body-parser')
 var jsonParser = bodyParser.json()
 var bcrypt = require('bcrypt')
-var db = require('../../services/db')
 var textUtils = require('./utilities/text')
 var cipher = require('./utilities/cipher')
 var { pwnedPassphrase } = require('./utilities/pwned')
+var { Secret } = require('../../services/db')
 
+router.post('/submit', jsonParser, async (req, res) => {
+  const METHODS = {
+    'aes': 0,
+    'des': 1,
+    'tripledes': 2,
+    'rabbit': 3,
+    'rc4': 4,
+    'rc4drop': 5
+  }
+  const DEFAULT_METHOD = 'aes'
+  const DEFAULT_EXPIRY = 86400 // 1 day
+  const MAX_EXPIRY = 604800 // 7 days
 
-router.post('/submit', jsonParser, (req, res) => {
-  res.status(501).send({})
+  try {
+    if (!req.body.body) throw 'Missing required body param: `body`.'
+    var body = textUtils.escape(req.body.body.toString())
+
+    if (!req.body.passphrase) throw 'Missing required body param: `passphrase`.'
+    var passphrase = req.body.passphrase.toString()
+
+    if (req.body.method && !METHODS.hasOwnProperty(req.body.method)) throw 'Param `method` must be one of: ' + Object.keys(METHODS).join(', ')
+    var method = req.body.method ? req.body.method : DEFAULT_METHOD
+
+    if (req.body.expiry && !Number.isInteger(parseInt(req.body.expiry)) || parseInt(req.body.expiry) < 0 || parseInt(req.body.expiry) > MAX_EXPIRY) throw 'Param `expiry` must be an integer between 0 and ' + MAX_EXPIRY
+    var expiryOffset = req.body.expiry ? parseInt(req.body.expiry) : DEFAULT_EXPIRY
+
+    var pwned = await pwnedPassphrase(passphrase)
+    if (pwned) throw 'Passphrase has been pwned (leaked online); please use something else.'
+
+    var expiryDate = new Date(Date.now() + expiryOffset*1000).toISOString()
+
+    var id = cipher.generateIdentifier()
+    //var encrypted_body = cipher.encrypt(body, passphrase, method)
+    var encrypted_body = cipher.encryptAesDemo(body, passphrase)
+    var hashed_passphrase = await bcrypt.hash(passphrase, 10).then(result => {
+      return result
+    })
+
+    if (Secret.create(id, encrypted_body, hashed_passphrase, method, expiryDate)) {
+      res.status(200).send({id: id})
+    } else {
+      res.status(400).send({error: "sql insertation error"})
+    }
+  } catch (err) {
+    res.status(400).send({error: err})
+  }
 })
 
-router.post('/:id', jsonParser, (req, res) => {
-  res.status(501).send({})
+router.post('/:id', jsonParser, async (req, res) => {
+  try {
+    if (!req.body.passphrase) throw 'Missing required body param: `passphrase`.'
+    var passphrase = req.body.passphrase.toString()
+
+    Secret.get(req.params.id, (row) => {
+      if (!row) throw 'Secret with that Id does not exist or has been deleted.'
+      
+      bcrypt.compare(passphrase, row.encrypted_passphrase, (err, result) => {
+        if (!result) throw 'Incorrect passphrase.'
+
+        var body = cipher.decryptAesDemo(row.store, passphrase)
+        Secret.destroy(row.id)
+        res.status(200).send({auth: body})
+      })
+    })
+  } catch (err) {
+    res.status(400).send({error: err})
+  }
 })
 
 router.delete('/:id', jsonParser, (req, res) => {
