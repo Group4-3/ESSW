5dff40f92ebfb7c52a64714c5dfcfd70f385a08b
Petri Bayley <petribayley@gmail.com>
2022-05-29 14:37:14 +1000

    Implimented pwnedpasswords check

--- docker_node_server/v1/submit_secret.js
+++ docker_node_server/v1/submit_secret.js
@@ -13,17 +13,35 @@ import * as secrets from '../modules/group43_secrets.js';
 import { crypto_encryptAes } from '../modules/group43_crypto.js';
 import bcrypt from 'bcrypt';
 import crypto from 'crypto-js';
+import fetch from 'node-fetch';
+
 var router = express.Router();
 
-router.post('/api/v1/secret/submit', (req, res) => {
+router.post('/api/v1/secret/submit', async (req, res) => {
     let secret_id = crypto.lib.WordArray.random(128/8).toString();
     let secret_text = req.body.secret_text;
     let passphrase = req.body.passphrase;
     let expiry_date = req.body.expiry_date//"05/06/2022 02:02:02";
     let method = req.body.encryption_method;
 
-    let passphrase_hashed = bcrypt.hashSync(secrets.pepper+passphrase, 10);
+    var pwn_sha1 = crypto.SHA1(passphrase).toString();
+    var pwn_prefix = pwn_sha1.substring(0, 5);
+    var pwn_suffix = pwn_sha1.substring(5);
+    var pwn_response = await fetch('https://api.pwnedpasswords.com/range/' + pwn_prefix, { method: 'get' });
+    var pwn_body = await pwn_response.text();
+    pwn_body = pwn_body.toLowerCase().replace(/:\d+/g, '').split("\r\n");
+    if(body.includes(pwn_suffix))
+    {
+        res.json({
+            def_res_url: req.url,
+            def_res_code: 400,
+            def_res_msg: "Password found ot be insecure",
+            msg_data: null
+        });
+        return;
+    }
 
+    let passphrase_hashed = bcrypt.hashSync(secrets.pepper+passphrase, 10);
     let secret_encrypted = crypto_encryptAes(secret_text, secrets.pepper+passphrase);
 
     let result = db.db_addSecret({
