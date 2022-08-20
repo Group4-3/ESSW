29b9682eb973218e1126e2ae595c06c4cf304013
Mitchell Thomas <6386297+mitchts@users.noreply.github.com>
2022-08-20 18:36:11 +1000

    Add /passphrase/generate tests example

---
+++ server/test/secret.test.js
@@ -0,0 +1,36 @@
+import supertest from 'supertest'
+import should from 'should'
+import { app } from '../app.js'
+
+describe("Test /passphrase", () => {
+  describe("GET /generate", () => {
+    it("should return a random passphrase", (done) => {
+      supertest(app)
+        .get('/api/v2/passphrase/generate')
+        .expect(200)
+        .expect((res) => {
+          return res.body.should.have.property('passphrase')
+        })
+        .end((err, res) => {
+          if (err) return done(err)
+          done()
+        })
+    })
+
+    it("should return a passphrase 12 characters long", (done) => {
+      supertest(app)
+        .get('/api/v2/passphrase/generate')
+        .send({
+          "length": 12
+        })
+        .expect(200)
+        .expect((res) => {
+          return res.body.should.have.property('passphrase').with.lengthOf(12)
+        })
+        .end((err, res) => {
+          if (err) return done(err)
+          done()
+        })
+    })
+  })
+})
