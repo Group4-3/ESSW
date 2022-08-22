402b398a83e91136069c1f5a079cbee6a924f71d
techno156 <techno156@hotmail.com>
2022-08-23 00:08:14 +1000

    Replace human-readable messages with binary success value. Separated error into simple reported string.

--- server/src/v2/modules/db.js
+++ server/src/v2/modules/db.js
@@ -68,9 +68,9 @@ function getStatement(preparedStatement, statementParams) {
     row = preparedStatement.get(statementParams);
   }
   catch (err) {
-    return { data: null, code: 500, human_code: `failure, ${err}` };
+    return { data: null, code: 500, error: `${err}`, success: false };
   }
-  return { data: row, code: 200, human_readable_code: "Success" };
+  return { data: row, code: 200, success: true };
 }
 
 function runStatement(preparedStatement, statementParams) {
@@ -79,9 +79,9 @@ function runStatement(preparedStatement, statementParams) {
     preparedStatement.run(statementParams);
   }
   catch (err) {
-    return { data: null, code: 500, human_code: `failure, ${err}` };
+    return { data: null, code: 500, error: `${err}`, success: false };
   }
-  return { data: null, code: 200, human_readable_code: "Success" };
+  return { data: null, code: 200, success: true };
 }
 
 //---
@@ -155,7 +155,7 @@ export function purgeExpiredSecrets() {
     var purgeInfo = PRUNE_SECRETS_QUERY.run(TABLE_NAME);
   }
   catch (err) {
-    return { data: null, code: 500, human_code: `failure, ${err}` };
+    return { data: null, code: 500, error: `${err}`, success: false };
   }
-  return { data: purgeInfo.changes, code: 200, human_readable_code: "Success" }; //Return the number of rows affected by purge
+  return { data: purgeInfo.changes, code: 200, success: true }; //Return the number of rows affected by purge
 }
