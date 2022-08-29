0cd3f9dd861707f3d22d5b86e3d127a5f6abfb70
Nimsara23 <102457217+Nimsara23@users.noreply.github.com>
2022-08-29 20:40:49 +1000

    initial commit

---
+++ client/src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return (
+<nav className="navbar">
+    <h1>Ephermeral Secret Sharing Website</h1>
+    <div className="links"> 
+       <a href="/">Home</a>
+       <a href="/viewsecret">View Secret</a>
+    </div>
+</nav>
+    );
+}
+
+export default Navbar;
