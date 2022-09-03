5741690c2cdfe2ef6336ad95cd1611f0041abae3
Petri Bayley <petribayley@gmail.com>
2022-09-03 23:19:20 +1000

    Change of directory and naming

--- nginx/nginx_noncertbot_gen.sh
+++ nginx/nginx_noncertbot_gen.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-if [ ! -f "/etc/ssl/${NGINX_HOST}/privkey.pem" ]; then
+if [ ! -f "/etc/ssl/${HOST_NAME}/fullchain.pem" ]; then
 echo "No Certificates, generating local - WARNING: Certificates will not be signed by an authorised CA";
-mkdir -p /etc/ssl/${NGINX_HOST}
-openssl ecparam -name prime256v1 -genkey -noout -out /etc/ssl/${NGINX_HOST}/privkey.pem
-openssl ec -in /etc/ssl/${NGINX_HOST}/privkey.pem -pubout -out /etc/ssl/${NGINX_HOST}/pubkey.pem
-openssl req -new -nodes -x509 -key /etc/ssl/${NGINX_HOST}/privkey.pem -in /etc/ssl/${NGINX_HOST}/pubkey.pem -out /etc/ssl/${NGINX_HOST}/certificate.pem -days 365 -subj "/C=AU/ST=GLOBAL/L=GLOBAL/O=ESSW/CN=GROUP4-3/OU=DEVOPS/"
-chown -R nginx:nginx /etc/ssl/${NGINX_HOST}/ 
-chmod 600 /etc/ssl/${NGINX_HOST}/*
+mkdir -p /etc/ssl/${HOST_NAME}
+openssl ecparam -name prime256v1 -genkey -noout -out /etc/ssl/${HOST_NAME}/privkey.pem
+openssl ec -in /etc/ssl/${HOST_NAME}/privkey.pem -pubout -out /etc/ssl/${HOST_NAME}/cert.pem
+openssl req -new -nodes -x509 -key /etc/ssl/${HOST_NAME}/privkey.pem -in /etc/ssl/${HOST_NAME}/cert.pem -out /etc/ssl/${HOST_NAME}/fullchain.pem -days 365 -subj "/C=AU/ST=GLOBAL/L=GLOBAL/O=ESSW/CN=GROUP4-3/OU=DEVOPS/"
+chown -R nginx:nginx /etc/ssl/${HOST_NAME}/ 
+chmod 600 /etc/ssl/${HOST_NAME}/*
 else
 echo "Certificates detected, not generating local certificates";
 fi
