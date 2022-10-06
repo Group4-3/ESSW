4fb0498ac8e995a6207f0b561317746fc9859402
Petri Bayley <petribayley@gmail.com>
2022-10-06 15:01:26 +1100

    Added headers

--- client/src/helpers/constants.js
+++ client/src/helpers/constants.js
@@ -1,3 +1,13 @@
+/*
+    ----- GROUP 43 Header -----
+    Component Name: group43_client_helpers_constants
+    Description: Constant variables for the client-side application.
+    Date of Creation: 29/9/2022
+    Author(s): Petri Bayley
+
+*/
+
+
 export function getApiAddress() {
 	// Ignoring Linter message because variable is external to javascript
 	// eslint-disable-next-line
---
+++ client/src/helpers/cryptography.js
@@ -0,0 +1,12 @@
+/*
+    ----- GROUP 43 Header -----
+    Component Name: group43_client_helpers_cryptography
+    Description: Helper library for client-side cryptography
+    Date of Creation: 6/10/2022
+    Author(s): Petri Bayley
+
+*/
+
+export function decryptUsingPrivateKey(buffer, privateKey) {
+	var crypt = new JSEncrypt();
+}
