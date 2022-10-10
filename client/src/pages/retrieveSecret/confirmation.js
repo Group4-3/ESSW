102955025cad4c00be15c2510d6db411cf968a9f
Petri Bayley <petribayley@gmail.com>
2022-10-11 01:45:22 +1100

    Added text area

--- client/src/pages/retrieveSecret/confirmation.js
+++ client/src/pages/retrieveSecret/confirmation.js
@@ -3,13 +3,11 @@ import {
   Link
 } from 'react-router-dom'
 
-
 const Confirmation = ({secretData}) => {
+
   return (
     <>
-      <h1>Your secret has been submitted</h1>
-      <p>URL: <Link to={'/secret/' + secretData.id}>{secretData.id}</Link></p>
-      <p>Expires At: {new Date(secretData.expires_at).toString()}</p>
+      <textarea rows='20' cols='80' readOnly={true}>{secretData.text}</textarea>
     </>
   );
 }
