a7bb36f1804951c56eba5b1d52953468ed956d0e
Petri Bayley <petribayley@gmail.com>
2022-10-11 01:44:49 +1100

    Added view secret heading with id

--- client/src/pages/retrieveSecret/index.js
+++ client/src/pages/retrieveSecret/index.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import Form from './form';
 import Confirmation from './confirmation';
+import {
+  useParams
+} from "react-router-dom";
 
 
 const RetrieveSecret = () => {
+  const { id } = useParams();
   const [secretData, updateSecretData] = React.useState(undefined);
   const formResponse = (data) => {
     updateSecretData(data)
   }
 
-  return secretData ? <Confirmation secretData={secretData}/> : <Form formResponse={formResponse}/>
+  return (
+    <>
+    <h1>View Secret [{id}]</h1>
+    {secretData ? <Confirmation secretData={secretData}/> : <Form formResponse={formResponse}/>}
+    </>
+    )
 }
 
 export default RetrieveSecret;
