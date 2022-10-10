768683d688646ea916325dbd50852770431017b2
Petri Bayley <petribayley@gmail.com>
2022-10-10 21:11:54 +1100

    Update index.js

--- client/src/pages/retrieveSecret/index.js
+++ client/src/pages/retrieveSecret/index.js
@@ -10,71 +10,91 @@ const RetrieveSecret = ({formResponse}) => {
   const [formData, updateFormData] = React.useState(Object.freeze({
     passphrase: '',
   }));
+  const [isPubkey] = React.useState(false);
 
-const handleInputChange = (e) => {
-  updateFormData({
-    ...formData,
-    [e.target.name]: e.target.value.trim()
-  });
-};
-
-
-
-const handleSubmit = async (e) => {
-  e.preventDefault();
-
-  try {
-    var formBody = [];
-    for (const [key, value] of Object.entries(formData)) {
-        var encodedKey = encodeURIComponent(key);
-        var encodedValue = encodeURIComponent(value);
-        formBody.push(encodedKey + "=" + encodedValue);
-    };
-    formBody = formBody.join("&");
-
-    let res = await fetch(Constants.getApiAddress() + '/api/v2/secret/' + id, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-      },
-      body: formBody
+  const handleInputChange = (e) => {
+    updateFormData({
+      ...formData,
+      [e.target.name]: e.target.value.trim()
     });
-
-    let json = await res.json();
-    if (res.status === 200) {
-      //formResponse(json);
-    } else {
-      // TODO error message should come from client validations
-      updateErrorMessage(JSON.stringify(json, null, 4))
-    }
-  } catch (err) {
-    console.log(err)
   };
-}
 
-return (
-  <>
-    <h1>Enter Secret ID</h1>
-    {errorMessage && (
-      <div className='alert alert-danger'>{errorMessage}</div>
-    )}
-  <form onSubmit={handleSubmit}>
-      
-    <div className='row g-3'>
-      <div id="files-list" className="row row-cols-4 g-1 mt-1"></div>
-      <div className="col">
-        <div className="row">
-          <label for="passphrase" className="col-sm-2 col-form-label">ID: </label>
-          <div className="col-sm-10">
-            <input type="password" id="passphrase" name="passphrase" onChange={handleInputChange} className="form-control"/>
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      var formBody = [];
+      for (const [key, value] of Object.entries(formData)) {
+          var encodedKey = encodeURIComponent(key);
+          var encodedValue = encodeURIComponent(value);
+          formBody.push(encodedKey + "=" + encodedValue);
+      };
+      formBody = formBody.join("&");
+
+      let res = await fetch(Constants.getApiAddress() + '/api/v2/secret/' + id, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        },
+        body: formBody
+      });
+
+      let json = await res.json();
+      if (res.status === 200) {
+        //formResponse(json);
+      } else {
+        // TODO error message should come from client validations
+        updateErrorMessage(JSON.stringify(json, null, 4))
+      }
+    } catch (err) {
+      console.log(err)
+    };
+  }
+
+  return (
+    <>
+      <h1>View Secret [{id}]</h1>
+      {errorMessage && (
+        <div className='alert alert-danger'>{errorMessage}</div>
+      )}
+    <form onSubmit={handleSubmit}>
+        
+      <div className='row g-3'>
+        <div id="files-list" className="row row-cols-4 g-1 mt-1"></div>
+        <div className="col">
+          <div className="row">
+            <label for="pubkey" className="col-sm-2 col-form-label">Passphrase</label>
+            <div className='col-sm-1'>
+              <div className='form-check form-switch row' style={{justifyContent: 'center'}}>
+                <input className='form-check-input' type='checkbox' id='pubkey' name='pubkey' onChange={e => isPubkey(e.target.checked)} style={{width: '50%'}} />
+              </div>
+            </div>
+            <label for="pubkey" className="col-sm-2 col-form-label">Public Key</label>
+          </div>
+          <div className="row">
+            <div className="col-sm-10">
+              {isPubkey === false &&
+                <>
+                  <label for="passphrase" className="col-sm-2 col-form-label">Passphrase</label>
+                  <input type="password" id="passphrase" name="passphrase" onChange={handleInputChange} className="form-control"/>
+                </>
+              }
+              {isPubkey === true &&
+                <>
+                  <label for="passphrase" className="col-sm-2 col-form-label">Private Key (Will never be sent to the server!)</label>
+                  <input type="password" id="passphrase" name="passphrase" onChange={handleInputChange} className="form-control"/>
+                </>
+              }
+            </div>
           </div>
         </div>
+        <button type="submit" className="btn btn-primary d-block w-100">Retrieve secret</button>
       </div>
-      <button type="submit" className="btn btn-primary d-block w-100">Retrieve secret</button>
-    </div>
-  </form>
-</>
-)
+    </form>
+  </>
+  )
 }
 
 export default RetrieveSecret;
