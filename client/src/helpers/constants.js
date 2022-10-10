b8a146503691b3d512d09d7e70bdc40a0e64a2bc
Mitchell Thomas <6386297+mitchts@users.noreply.github.com>
2022-10-10 21:03:01 +1100

    UI changes to public key segment

--- client/src/helpers/constants.js
+++ client/src/helpers/constants.js
@@ -4,14 +4,12 @@
     Description: Constant variables for the client-side application.
     Date of Creation: 29/9/2022
     Author(s): Petri Bayley
-
 */
-
 
 export function getApiAddress() {
 	// Ignoring Linter message because variable is external to javascript
 	// eslint-disable-next-line
-	const API_HOST_NAME = "${API_HOST_NAME}"
+	const API_HOST_NAME = '${API_HOST_NAME}'
 	const regex = /(\$\{.*\})/
 	return (regex.test(API_HOST_NAME) ? 'http://localhost:3001' : 'https://' + API_HOST_NAME)
 }
