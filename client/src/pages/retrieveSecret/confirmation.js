6acff95e2678033348148b6a74dcd4e72df20d6b
Petri Bayley <petribayley@gmail.com>
2022-10-11 11:27:15 +1100

    Display files

--- client/src/pages/retrieveSecret/confirmation.js
+++ client/src/pages/retrieveSecret/confirmation.js
@@ -2,12 +2,32 @@ import React from 'react';
 import {
   Link
 } from 'react-router-dom'
+import { getFileIcon, humanReadableSize } from '../../helpers/file';
 
 const Confirmation = ({secretData}) => {
 
   return (
     <>
+      <div id='files-list' className='row row-cols-4 g-1 mt-1'>
+        { secretData.files.map((element, index) => {
+          var file = new File(element.blob.data, element.file_name, {type: element.mimetype });
+
+          return (
+          <div class='col'>
+            <div class='card h-100'>
+              <div class='card-body d-flex flex-column align-items-center'>
+                <i class='bi ${getFileIcon(element.mimetype)} h1'></i>
+                <span class='font-monospace small'>{element.file_name}</span>
+                <span class='text-muted small fw-bold pt-2'>{humanReadableSize(element.size)}</span>
+                <a href={URL.createObjectURL(file)} download={element.file_name}>Download</a>
+              </div>
+            </div>
+          </div>
+        )})
+        }
+      </div>
       <textarea rows='20' cols='80' readOnly={true}>{secretData.text}</textarea>
+        }
     </>
   );
 }
